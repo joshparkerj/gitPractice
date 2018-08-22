c6da5365e75f54cdbb3563f37bf63cfa55ea85e4
joshparkerj <superfakek@gmail.com>
2018-08-21 19:37:42 -0600

    log my name that many times

--- practice.js
+++ practice.js
@@ -2,6 +2,14 @@ const response = 2 * 4;
 
 console.log(`The response is ${response}`);
 
-const responseDoubled = 2 * response;
+let responseDoubled = 2 * response;
 
 console.log(`Twice the response is ${responseDoubled}`);
+
+const name = 'Josh';
+const names = [];
+while (responseDoubled--){
+  names.push(name);
+}
+
+console.log(`my name that many times is ${names.join()}`)
